819469578d797ffd272e52a8104512ac6665eea3
dspeckhard <dspeckhard@bcj.com>
2015-12-30 10:43:58 -0800

    update to Java 8, tomcat 8

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -9,26 +9,28 @@ apt-get -y install curl
 # You can install anything you need here.
 
 #	Install JRE 7 and Tomcat7, plus ntpdate
-apt-get -y install openjdk-7-jre-headless tomcat7 ntpdate tzdata wget
+apt-get -y install openjdk-8-jre-headless tomcat8 ntpdate tzdata wget
 
 #Set timezone and time
-echo "US/Pacific" > /etc/timezone
+rm -rf /etc/timezone
+echo "America/Los_Angeles" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 ntpdate 0.nl.pool.ntp.org
 
 #Create a directory for you BIMserver home directory (this has nothing to do with /home on unix systems
 mkdir /var/bimserver
 #Give the appropriate rights to the tomcat7 user
-chown -R tomcat7 /var/bimserver
+chown -R tomcat8 /var/bimserver
 
 #	Go to tomcat folder
-cd /var/lib/tomcat7/webapps
+cd /var/lib/tomcat8/webapps
 
 # Get rid of "default" webapp
 rm -rf ROOT/
 
 # Download the latest BIMserver (Make sure you replace this with the latest version!)
 wget https://github.com/opensourceBIM/BIMserver/releases/download/1.4.0-FINAL-2015-11-04/bimserver-1.4.0-FINAL-2015-11-04.war -O ROOT.war
+chown tomcat8 ROOT.war
 
 # start tomcat7
-# service tomcat7 restart
+service tomcat8 restart
